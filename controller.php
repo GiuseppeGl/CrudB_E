5295bac82f5c6516588f5e092f66ba8bc83b3b0f
Giuseppe Longo <longorsm68@gmail.com>
2024-03-11 09:08:44 +0100

    fine

--- controller.php
+++ controller.php
@@ -4,54 +4,47 @@ session_start();
 // Include the necessary files
 require_once("assets/php/classes/UsersDto.php");
 require_once("assets/php/db.php");
+require_once("assets/php/classes/Users.php");
 $config = require_once("assets/php/config.php");
 use db\DB_PDO as Database;
 use dto\UserDTO as Dto;
-
-
-// $pdoConn = Database::getInstance($config);
-// $conn = $pdoConn->getConnection();
-
-// $userManager = new Dto($conn);
-
-
-// var_dump($_POST);
-// exit();
+use Users\NormalUser as UtenteNormale;
 
 
 
-// Check if the form was submitted via POST and the operation is 'newuser'.
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_REQUEST['mode'] === 'newUser'){
-    // Initialize the PDO connection using the Database class
+
+    
+
     $pdoConn = Database::getInstance($config);
     $conn = $pdoConn->getConnection();
 
-    // Instantiate the UserDTO object with the PDO connection
     $userManager = new Dto($conn);
 
-    // Prepare the user data array from the POST data.
+    $isAdmin = 0;
+
+    if(isset($_POST['isAdmin'])) {
+        $isAdmin = 1;
+    } 
+
     $userData = [
         'Nome' => $_POST['nome'],
         'Cognome' => $_POST['cognome'],
         'City' => $_POST['city'],
         'email' => $_POST['email'],
+        'img' => $_POST['img'],
+        'isAdmin' => $isAdmin,
         'Password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
     ];
 
-
-
-    // Call the saveUser function with the user data.
     $result = $userManager->saveUser($userData);
 
 
 
-    // Check if the user was saved successfully.
     if ($result > 0) {
-        // User was saved, redirect or display a success message.
         echo "User saved successfully!";
-        exit(header("Location:index.php"));
+        exit(header("Location:login.php"));
     } else {
-        // User was not saved, handle the error.
         echo "An error occurred while saving the user.";
         exit(header("Location:register.php?error=true"));
     }
@@ -84,6 +77,13 @@ if ($_REQUEST["mode"] === 'login') {
             $_SESSION["nome"] = $utente['Nome'];
             $_SESSION["cognome"] = $utente['Cognome'];
             $_SESSION["email"] = $utente['email'];
+            $_SESSION["img"] = $utente['img'];
+
+            if($utente['isAdmin'] === 1) {
+                $_SESSION["isAdmin"] = true; 
+            }else {
+                $_SESSION["isAdmin"] = false;  
+            };
 
             // Dopo aver impostato le variabili di sessione, reindirizza all'indice
             exit(header('Location: index.php'));
@@ -97,3 +97,88 @@ if ($_REQUEST["mode"] === 'login') {
     }
 }
 
+/* Logout*/
+
+if ($_REQUEST["mode"] === 'logout') {
+    session_destroy();
+    session_write_close();
+    exit(header('Location: register.php'));
+}
+
+
+
+
+
+// update user info 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_REQUEST['mode'] === 'updateInfo'){
+
+    
+
+    $pdoConn = Database::getInstance($config);
+    $conn = $pdoConn->getConnection();
+
+    $userManager = new Dto($conn);
+
+
+    
+
+    $userData = [
+        'Nome' => $_POST['nome'],
+        'Cognome' => $_POST['cognome'],
+        'City' => $_POST['city'],
+        'email' => $_POST['email'],
+        'img' => $_POST['img'],
+        'id' => $_SESSION['actualUpdateId']
+    ];
+
+    $result = $userManager->updateUser($userData);
+
+
+
+    if ($result > 0) {
+        echo "User saved successfully!";
+        exit(header("Location:index.php"));
+    } else {
+        echo "An error occurred while saving the user.";
+    }
+};
+
+
+
+
+// Delete user function 
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_REQUEST['mode'] === 'deleteUser'){
+
+    
+
+    $pdoConn = Database::getInstance($config);
+    $conn = $pdoConn->getConnection();
+
+    $userManager = new Dto($conn);
+
+    $idUtente = $_REQUEST['id'];
+
+    // var_dump($_SESSION);
+
+   
+
+    if($_SESSION['isAdmin'] == false && $_SESSION['user_id'] != $_REQUEST['id']) {
+        echo "<h1 class='m-5'>Sei un h4k3rino???? Prof nun ce provà</h1>";
+        exit();
+    } else {
+        echo "ciao sono dentro";
+        $result = $userManager->deleteUser($idUtente);
+        echo "ho fatto delete";
+        if ($result > 0) {
+            echo "User saved successfully!";
+            exit(header("Location:index.php"));
+        } else {
+            echo "An error occurred while saving the user.";
+        }
+
+    }
+   
+};
--- index.php
+++ index.php
@@ -2,24 +2,68 @@
 session_start();
 if ($_SESSION['login'] !== 'true') {
     // Reindirizza l'utente alla pagina di login
-    header('Location: login.php');
+    header('Location: register.php');
     exit; // Assicurati di chiamare exit dopo il reindirizzamento per fermare l'esecuzione dello script
 }
 session_write_close();
 ?>
 
-<?php include_once "assets/php/header.php"; ?>
+<?php include_once "assets/php/header.php";
+require_once "assets/php/classes/UsersDto.php";
+require_once "assets/php/db.php";
+$config = require_once "assets/php/config.php";
+use db\DB_PDO as Database;
+use dto\UserDTO as Dto;
+
+$pdoConn = Database::getInstance($config);
+$conn = $pdoConn->getConnection();
+$usersCard = new Dto($conn);
+$res = $usersCard->getAll();
+?>
 
 
+<div class="mt-5 row row-cols-1 row-cols-md-3 g-4 px-5">
 
+    <?php if ($res) {
+        foreach ($res as $users) {
+            ?>
+            <div class="col">
+                <div class="card">
+                    <?php if ($users['img']) {
+                        echo "<img src='" . $users['img'] . "' class='card-img-top m-auto' alt='...'>";
 
+                    } else {
+                        echo '<img src="assets/img/crudo.png" class="card-img-top m-auto" alt="...">';
+                    }
+                    ; ?>
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?= $users['Nome'] . " " . $users['Cognome'] ?>
+                        </h5>
+                        <p class="card-text">Email:
+                            <?= $users['email'] ?>
+                        </p>
+                        <p class="card-text">Città:
+                            <?= $users['City'] ?>
+                        </p>
+                        <p class="card-text" hidden>
+                            <?= $users['ID'] ?>
+                        </p>
+                        <?php if ($_SESSION['user_id'] === $users['ID'] || $_SESSION['isAdmin'] === true) {
+                            echo "<form action='update.php' method='get' class='d-inline'>
+                            <input type='hidden' name='idUtente' value='".$users['ID']."'>
+                            <button type='submit' class='btn btn-primary w-25'>Update Info</button>
+                        </form>
+                        <form action='controller.php?mode=deleteUser&id=".$users['ID']."' method='post' class='d-inline'>
+                            <input type='hidden' name='idUtente' value='".$users['ID']."'>
+                            <button type='submit' class='btn btn-primary w-25'>Delete User</button>
+                        </form>";
+                        } ?>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+    } ?>
 
-
-
-
-
-
-
-
-
-<?php include_once "assets/php/footer.php"; ?>
+    <?php include_once "assets/php/footer.php"; ?>
